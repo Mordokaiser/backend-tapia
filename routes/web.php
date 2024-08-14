aa27caad18baa0430d3f912628a8190f9de70727
fpinilla198 <148462195+fpinilla198@users.noreply.github.com>
2024-08-14 10:56:39 -0400

    se corrige el como se llamaba a la ruta de api
    se corre comando php artisan install:api
    y se copia y pega la ruta de la api que guarda los telefonos del archivo de rutas web, al archivo de rutas api

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PhonesController;
-
 Route::get('/', function () {
     return view('welcome');
 });
-//ruta para guardar un registro de telefono
-Route::get('add-phones', [PhonesController::class, 'store']);
+
